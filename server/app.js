5614da1957dbaa00231d4c580a0ebb5388945bf5
241GraphiK <josephekangaedzeghe@gmail.com>
2026-01-16 00:09:01 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,11 @@ const Result = require("./models/Result");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+// Autoriser uniquement ton GitHub Pages
+app.use(cors({
+  origin: "https://241graphik.github.io/QCM-App/"
+}));
 
 // Connexion MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
